c7ae09bb6b08627f0b4506a38a3a43ab52560494
alvazar <alvazar@gmail.com>
2021-02-11 15:18:17 +0300

    update

---
+++ App/Config/Settings_example.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Config;
+
+/* need rename file to Settings.php */
+
+final class Settings
+{
+    //
+    public const DATA_TABLE = "";
+    public const PROJECT_ID = "";
+    
+    // paths
+    public const ROOT_DIR = '/';
+
+    //
+    public const USER_ID = 0; // 0 - not authorized
+}
---
+++ App/Config/Storage_example.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Config;
+
+/* need rename file to Storage.php */
+
+final class Storage
+{
+    // database settings
+    public const DB_HOST = "lifebook.ww";
+    public const DB_USER = "root";
+    public const DB_PASSW = "root";
+    public const DB_NAME = "lifebook";
+    public const DB_SOCKET = "";
+
+    // memcache settings
+    public const MC_HOST = "prog.ww";
+    public const MC_PORT = "11211";
+}
